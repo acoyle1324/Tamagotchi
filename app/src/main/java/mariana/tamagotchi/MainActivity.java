c0676d93b632c7a6ac31f22ea10de3a9bc5958b2
Mariana <marianacazaresm@gmail.com>
2018-05-21 18:28:13 -0700

    Gatito and Main Activity changes

--- app/src/main/java/mariana/tamagotchi/MainActivity.java
+++ app/src/main/java/mariana/tamagotchi/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Gatito gatito;
+    private static int com = 100, ener = 100, feli = 100, limp = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
